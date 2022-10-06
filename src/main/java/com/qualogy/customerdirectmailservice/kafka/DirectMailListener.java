a4fe40fc7884db2cb1778a2217fa8a5d582f62a7
Ronald Bakker <ronald.bakker@ah.nl>
2022-10-06 17:09:42 +0200

    Added Kafka

---
+++ src/main/java/com/qualogy/customerdirectmailservice/kafka/DirectMailListener.java
@@ -0,0 +1,15 @@
+package com.qualogy.customerdirectmailservice.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DirectMailListener {
+
+    @KafkaListener(topics = "quickstart-events")
+    public void listenForDirectMail(String message) {
+        System.out.println("Received Message in group foo: " + message);
+    }
+}
+
+
